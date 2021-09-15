7c6fbedf3c812fb62241d0135724a8f0db40753d
SAID El ABOUDI <saidelaboudi3@gmail.com>
2021-09-15 02:09:50 +0100

    contact us was uploaded !

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,9 +1,22 @@
+import axios from 'axios';
 import React from 'react';
 
+const HOST = 'http://localhost:8095';
+
 const ContactUs = () => {
-    return(
-        <section id="contact-us" class="contact-us section">
-		<div class="container">
+	var contact = {
+		"id": 1,
+		"phoneNumber": "+212 573 904 066",
+		"email": "said@support.com",
+		"address": "adress xy ville z",
+		"aboutUs": "Nous somme xyz"
+	};
+	axios.get(HOST + '/api/contactUs/get').then((response) => {
+		contact = response.data
+	});
+	return (
+		<section id="contact-us" class="contact-us section">
+			<div class="container">
 				<div class="contact-head">
 					<div class="row">
 						<div class="col-lg-8 col-12">
@@ -17,26 +30,26 @@ const ContactUs = () => {
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>Your Name<span>*</span></label>
-												<input name="name" type="text" placeholder=""/>
+												<input name="name" type="text" placeholder="" />
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>Your Subjects<span>*</span></label>
-												<input name="subject" type="text" placeholder=""/>
+												<input name="subject" type="text" placeholder="" />
 											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>Your Email<span>*</span></label>
-												<input name="email" type="email" placeholder=""/>
-											</div>	
+												<input name="email" type="email" placeholder="" />
+											</div>
 										</div>
 										<div class="col-lg-6 col-12">
 											<div class="form-group">
 												<label>Your Phone<span>*</span></label>
-												<input name="company_name" type="text" placeholder=""/>
-											</div>	
+												<input name="company_name" type="text" placeholder="" />
+											</div>
 										</div>
 										<div class="col-12">
 											<div class="form-group message">
@@ -59,23 +72,21 @@ const ContactUs = () => {
 									<i class="fa fa-phone"></i>
 									<h4 class="title">Call us Now:</h4>
 									<ul>
-										<li>+123 456-789-1120</li>
-										<li>+522 672-452-1120</li>
+										<li>{contact.phoneNumber}</li>
 									</ul>
 								</div>
 								<div class="single-info">
 									<i class="fa fa-envelope-open"></i>
 									<h4 class="title">Email:</h4>
 									<ul>
-										<li><a href="mailto:info@yourwebsite.com">info@yourwebsite.com</a></li>
-										<li><a href="mailto:info@yourwebsite.com">support@yourwebsite.com</a></li>
+										<li><a href="mailto:`contact.email`">{contact.email}</a></li>
 									</ul>
 								</div>
 								<div class="single-info">
 									<i class="fa fa-location-arrow"></i>
 									<h4 class="title">Our Address:</h4>
 									<ul>
-										<li>KA-62/1, Travel Agency, 45 Grand Central Terminal, New York.</li>
+										<li>{contact.address}</li>
 									</ul>
 								</div>
 							</div>
@@ -83,8 +94,8 @@ const ContactUs = () => {
 					</div>
 				</div>
 			</div>
-	</section>
-    )
+		</section>
+	)
 }
 
 export default ContactUs;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,20 @@
+import axios from 'axios';
 import React from 'react';
 
+const HOST = 'http://localhost:8095';
+
 const Footer = () => {
+    var contact = {
+        "id": 1,
+        "phoneNumber": "+212 573 904 066",
+        "email": "said@support.com",
+        "address": "adress xy ville z",
+        "aboutUs": "Nous somme xyz"
+    };
+    axios.get(HOST + '/api/contactUs/get').then((response) => {
+        contact = response.data
+    });
+
     return (
         <footer className="footer">
             <div className="footer-top section">
@@ -11,8 +25,8 @@ const Footer = () => {
                                 <div className="logo">
                                     <a href="index.html"><img src="images/logo2.png" alt="#" /></a>
                                 </div>
-                                <p className="text">Praesent dapibus, neque id cursus ucibus, tortor neque egestas augue,  magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus.</p>
-                                <p className="call">Got Question? Call us 24/7<span><a href="tel:123456789">+0123 456 789</a></span></p>
+                                <p className="text">{contact.phoneNumber}</p>
+                                <p className="call">Got Question? Call us 24/7<span><a href="tel:123456789">{contact.phoneNumber}</a></span></p>
                             </div>
                         </div>
                         <div className="col-lg-2 col-md-6 col-12">
@@ -22,7 +36,7 @@ const Footer = () => {
                                     <li><a href="home">About Us</a></li>
                                     <li><a href="home">Faq</a></li>
                                     <li><a href="home">Terms & Conditions</a></li>
-                                    <li><a href="home">Contact Us</a></li>
+                                    <li><a href="contactUs">Contact Us</a></li>
                                     <li><a href="home">Help</a></li>
                                 </ul>
                             </div>
@@ -44,10 +58,9 @@ const Footer = () => {
                                 <h4>Get In Tuch</h4>
                                 <div className="contact">
                                     <ul>
-                                        <li>NO. 342 - London Oxford Street.</li>
-                                        <li>012 United Kingdom.</li>
-                                        <li>info@eshop.com</li>
-                                        <li>+032 3456 7890</li>
+                                        <li>{contact.address}</li>
+                                        <li>{contact.email}</li>
+                                        <li>{contact.phoneNumber}</li>
                                     </ul>
                                 </div>
                                 <ul>
@@ -56,24 +69,6 @@ const Footer = () => {
                                     <li><a href="home"><i className="ti-flickr"></i></a></li>
                                     <li><a href="home"><i className="ti-instagram"></i></a></li>
                                 </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="copyright">
-                <div className="container">
-                    <div className="inner">
-                        <div className="row">
-                            <div className="col-lg-6 col-12">
-                                <div className="left">
-                                    <p>Copyright © 2020 <a href="http://www.wpthemesgrid.com">Wpthemesgrid</a>  -  All Rights Reserved.</p>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-12">
-                                <div className="right">
-                                    <img src="images/payments.png" alt="#" />
-                                </div>
                             </div>
                         </div>
                     </div>
